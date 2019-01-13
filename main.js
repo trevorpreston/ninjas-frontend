6d56852dcba830bdac3e999290709179678450aa
Jackie Casper <casper.jaclyn@gmail.com>
2019-01-13 14:20:44 +0300

    add random attack points

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
-['⭐️', '⭐️', '⭐️', '⭐️'].forEach( throwingStar => {
-  console.log(`${throwingStar} hit ninja Trevor!`)
-} )
+const minAttack = 0;
+const maxAttack = 60;
+
+const randomAttack = () => {
+  return minAttack + (Math.floor(Math.random() * maxAttack))
+}
+
+const stars = ['⭐️', '⭐️', '⭐️', '⭐️'];
+
+stars.forEach(throwingStar => {
+  const attack = randomAttack();
+
+  console.log(`${throwingStar} hit ninja Trevor for ${attack} points!`);
+
+})
+
