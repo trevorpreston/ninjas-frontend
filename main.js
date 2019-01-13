50f0cc468e715970597d26f5ac40dbc739d8ed67
Jackie Casper <casper.jaclyn@gmail.com>
2019-01-13 14:21:29 +0300

    add health tracking

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const minAttack = 0;
 const maxAttack = 60;
 
+let trevorsHP = 100;
+
 const randomAttack = () => {
   return minAttack + (Math.floor(Math.random() * maxAttack))
 }
@@ -9,8 +11,14 @@ const stars = ['⭐️', '⭐️', '⭐️', '⭐️'];
 
 stars.forEach(throwingStar => {
   const attack = randomAttack();
-
-  console.log(`${throwingStar} hit ninja Trevor for ${attack} points!`);
-
+  trevorsHP -= attack;
+  if (trevorsHP <= 0) {
+    console.log(`Trevor is dead!`);
+  } else {
+    console.log(`${throwingStar} hit ninja Trevor for ${attack} points! His health is at ${trevorsHP}!`);
+  }
 })
 
+if (trevorsHP > 0) {
+  console.log(`Trevor survived with ${trevorsHP} health!!`)
+}
